1db73af9884706c3e37717723b55d5f14796df71
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-01-26 08:21:01 -0300

    second commit

--- exe074 - Maior e menor valor em tupla.py
+++ exe074 - Maior e menor valor em tupla.py
@@ -3,11 +3,33 @@ from random import randint
 maiorvalor = 0
 menorvalor = 0
 
-a = ' {}'.format(randint(0, 9))
-b = ' {}'.format(randint(0, 9))
-c = ' {}'.format(randint(0, 9))
-d = ' {}'.format(randint(0, 9))
-e = ' {}'.format(randint(0, 9))
+a = int(' {}'.format(randint(0, 9)))
+if a > maiorvalor:
+    maiorvalor = a
+if menorvalor == 0:
+    menorvalor = a
+b = int(' {}'.format(randint(0, 9)))
+if b > maiorvalor:
+    maiorvalor = b
+if b < menorvalor:
+    menorvalor = b
+c = int(' {}'.format(randint(0, 9)))
+if c > maiorvalor:
+    maiorvalor = c
+if c < menorvalor:
+    menorvalor = c
+d = int(' {}'.format(randint(0, 9)))
+if d > maiorvalor:
+    maiorvalor = d
+if d < menorvalor:
+    menorvalor = d
+e = int(' {}'.format(randint(0, 9)))
+if e > maiorvalor:
+    maiorvalor = e
+if e < menorvalor:
+    menorvalor = e
 f = (f'{a}', f'{b}', f'{c}', f'{d}', f'{e}')
-print(f)
-print(type(f))
+
+print(f'Os números sorteados foram: {f}')
+print(f'O menor valor foi {menorvalor}')
+print(f'O maior valor foi {maiorvalor}')
