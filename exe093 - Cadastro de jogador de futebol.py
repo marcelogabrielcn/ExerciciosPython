6a87d89c10029f1abe9270587cdfafe984c49dd0
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-02-26 08:00:29 -0300

    Primeiro Commit

---
+++ exe093 - Cadastro de jogador de futebol.py
@@ -0,0 +1,22 @@
+dici = {}
+gols = []
+tot = 0
+
+dici['nome'] = str(input('Digite o nome do jogador: '))
+dici['partidas'] = int(input(f'Quantas partidas {dici["nome"]} jogou essa temporada? '))
+for g in range(0, dici["partidas"]):
+    gols.append(int(input(f'Quantos gols o jogador fez no jogo {g}? ')))
+    dici['gols'] = gols
+for gol in gols:
+    tot += gol
+dici['total'] = tot
+print('-=-' * 10)
+print(dici)
+print('-=-' * 10)
+for k, v in dici.items():
+    print(f'O campo {k} tem valor {v}')
+print('-=-' * 10)
+print(f'O jogador {dici["nome"]} jogou ao todo {dici["partidas"]} partidas')
+for i, v in enumerate(gols):
+    print(f'   => No jogo {v+1} fez {v} gols')
+print('fim')
