c8a5c0c3b9e6004c172fa2a41ad8ce668c57857b
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-03-12 07:22:23 -0300

    Segundo Commit
    Finalizando os últimos requisitos

--- exe094 - Unindo dicionarios e listas.py
+++ exe094 - Unindo dicionarios e listas.py
@@ -1,10 +1,13 @@
 pessoa = {}
 pessoas = []
 medIdade = 0
+mulheres = []
 
 while True:
     pessoa['nome'] = str(input('Nome: ')).strip().capitalize()
     pessoa['sexo'] = str(input('Sexo (M/F): ')).strip()[0].upper()
+    if pessoa['sexo'] == "F":
+        mulheres.append(pessoa['nome'][:])
     pessoa['idade'] = int(input('Idade: '))
     medIdade += pessoa['idade']
     pessoas.append(pessoa.copy())
@@ -19,3 +22,9 @@ while True:
 print('-=-' * 10)
 print(f'Ao todo foram cadastradas {len(pessoas)} pessoas.')
 print(f'A média de idade das pessoas é {medIdade/len(pessoas):.1f} anos.')
+print(f"As mulheres cadastradas foram: {mulheres}")
+print(f"As pessoas acima da média de idade são: ")
+for i, v in enumerate(pessoas):
+    if v['idade'] > medIdade/len(pessoas):
+        print(v['nome'])
+print('Fim do programa')
