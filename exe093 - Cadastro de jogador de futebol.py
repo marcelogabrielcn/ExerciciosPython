cb506d675d45a4854d01116805996995af0111db
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-03-12 07:25:49 -0300

    Segundo Commit
    Correções de lógica

--- exe093 - Cadastro de jogador de futebol.py
+++ exe093 - Cadastro de jogador de futebol.py
@@ -5,7 +5,7 @@ tot = 0
 dici['nome'] = str(input('Digite o nome do jogador: '))
 dici['partidas'] = int(input(f'Quantas partidas {dici["nome"]} jogou essa temporada? '))
 for g in range(0, dici["partidas"]):
-    gols.append(int(input(f'Quantos gols o jogador fez no jogo {g}? ')))
+    gols.append(int(input(f'Quantos gols o jogador fez no jogo {g+1}? ')))
     dici['gols'] = gols
 for gol in gols:
     tot += gol
@@ -18,5 +18,5 @@ for k, v in dici.items():
 print('-=-' * 10)
 print(f'O jogador {dici["nome"]} jogou ao todo {dici["partidas"]} partidas')
 for i, v in enumerate(gols):
-    print(f'   => No jogo {v+1} fez {v} gols')
+    print(f'   => No jogo {i+1} fez {v} gols')
 print('fim')
