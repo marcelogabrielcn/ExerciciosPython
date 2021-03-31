f99c39f989192f757e36af64cbf63e0ac5fb77f1
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-03-31 20:54:22 -0300

    Terceiro Commit
    Finalizando

--- exe095 - Aprimorando os dicionarios.py
+++ exe095 - Aprimorando os dicionarios.py
@@ -7,10 +7,11 @@ while True:
     dici['partidas'] = int(input(f'Quantas partidas {dici["nome"]} jogou essa temporada? '))
     for g in range(0, dici["partidas"]):
         gols.append(int(input(f'Quantos gols o jogador fez no jogo {g+1}? ')))
-        dici['gols'] = gols
+        dici['gols'] = gols.copy()
     dici['total'] = sum(gols)
     jogadores.append(dici.copy())
     dici.clear()
+    gols.clear()
     continuar = str(input('Quer continuar? (S/N) ')).upper()[0].strip()
     if continuar not in 'SN':
         continuar = str(input('Quer continuar? (S/N) ')).upper()[0].strip()
@@ -18,11 +19,24 @@ while True:
         break
 
 print('-=-' * 30)
-print(jogadores)
-print('Cod.  Nome        Gols           Total')
+print('Cod.  Nome        Partidas          Gols           Total')
 for k, v in enumerate(jogadores):
     print(f'{k:<6}', end='')
     for d in v.values():
         print(f'{str(d):<15}', end='')
     print()
+
+print('-=-' * 30)
+while True:
+    pesquisa = int(input('Quer ver os dados de qual jogador? (999 para parar) '))
+    if pesquisa == 999:
+        break
+    if pesquisa >= len(jogadores) or pesquisa < 0:
+        print(f'Oops, parece que não existe esse jogador. ')
+    else:
+        print(f'Levantamento do jogador {jogadores[pesquisa]["nome"]}: ')
+        for i, g in enumerate(jogadores[pesquisa]["gols"]):
+            print(f'    No jogo {i+1} fez {g} gols')
+        print('-=-' * 30)
+
 print('Fim do Programa')
