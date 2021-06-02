0ba52fb4d834f8ea7a5816dc289acbc502db96f6
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-06-02 20:57:37 -0300

    Segundo Commit

--- exe096 - Função que calcula área.py
+++ exe096 - Função que calcula área.py
@@ -1,10 +1,10 @@
 def area(a, b):
     area = a * b
-    print(f'\nA área desse do terreno {a}x{b} é igual a {area}m²')
+    print(f'\nA área desse do terreno {a} x {b} é igual a {area}m²')
 
 
 print('Controle de terrenos')
 print('-=-' * 7)
-lar = float(input('Digite a largura do terreno (M): '))
-comp = float(input('Digite o comprimento do terreno (M): '))
+lar = float(input('Digite a largura do terreno (m): '))
+comp = float(input('Digite o comprimento do terreno (m): '))
 area(lar, comp)
