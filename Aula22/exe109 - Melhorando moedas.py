90dfd2b78b163eb829c70cec26c4be3c4941fe59
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-07-15 09:44:30 -0300

    Segundo commit

--- Aula22/exe109 - Melhorando moedas.py
+++ Aula22/exe109 - Melhorando moedas.py
@@ -3,5 +3,5 @@ import moeda
 num = float(input('Informe um valor: '))
 print(f'A metade desse valor é: {moeda.metade(num, True)}')
 print(f'O dobro desse valor é: {moeda.dobro(num, True)}')
-print(f'Aumentando o valor em 10% temos: {moeda.aumentar(num)}')
-print(f'Diminuindo o valor em 10% temos: {moeda.diminuir(num)}')
+print(f'Aumentando o valor em 10% temos: {moeda.aumentar(num, True)}')
+print(f'Diminuindo o valor em 10% temos: {moeda.diminuir(num, True)}')
